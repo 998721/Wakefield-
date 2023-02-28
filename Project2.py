933bfe4432409ed745e00fecd6402ebe3ecd01bf
998721 <125505728+998721@users.noreply.github.com>
2023-02-28 19:26:39 +0000

    add

---
+++ Project2.py
@@ -0,0 +1,27 @@
+print ("Hello, world! This is Python 3!")
+
+# Hello, my name is Isaac Doll and this codes solves the formula for the surface area of a rectangular prism.
+
+print ("This code will give you the area of a rectangle. To follow the formula you'll need to input a length, width and height")
+
+#For example, if your length is 4 inches, width 9 inches, and height 41 inches. the answer would be 1138in squared
+
+import math
+
+print("type in three numbers")
+
+length = input("what's the length") 
+width = input("what's the width")
+height = input("what's the height")
+
+L = length 
+W = width 
+H = height 
+
+# Now that you have your inputs just hit the return key and let the code solve the equation. The code runs through the formula using PEMDAS and solves it.
+
+result = 2*(W*L + H*L + H*W)
+
+print(result)
+
+# if its right then congrats you've got the formula for a rectangular prism.
